2e4ccb9de4adcdcd107fe5c8bb56f1131293e18d
Bilal IGCI <70436168+bigci10@users.noreply.github.com>
2022-04-01 20:30:50 +0300

    Update Solutions.java

--- Secondweek/Solutions.java
+++ Secondweek/Solutions.java
@@ -5,7 +5,7 @@ public class Solutions {
          * 2 ile tam bölünenleri ekrana yazdır.
          */
 
-        for (int i = 2; i <= 11; i+=2)
+        for (int i = 2; i <= 10; i+=2)
         {  
             System.out.println(i);
         }
