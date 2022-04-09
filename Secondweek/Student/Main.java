1b0e39df68e7caf69cf4699a1193aa4fb44d587f
Bilal IGCI <70436168+bigci10@users.noreply.github.com>
2022-04-09 04:46:32 -0400

    Update Main.java

--- Secondweek/Student/Main.java
+++ Secondweek/Student/Main.java
@@ -8,16 +8,16 @@
 class Main {
     public static void main(String[] args) {
 
-        Student student = new Apple("5403633123","Bilal","Man");
-        Student student1 = new Apple("5079705123","Fatih","Man");
-        Student student2 = new Apple("5424363123","Gizem","Woman");
-        Student student3 = new Apple("5414322123","Büşra","Woman");
-        Student student4 = new Apple("5403633123","Ahmet","Man");
-        Student student5 = new Apple("5079705123","Mustafa","Man");
-        Student student6 = new Apple("5424363123","Merve","Woman");
-        Student student7 = new Apple("5414322123","Ceyda","Woman");
-        Student student8 = new Apple("5414322123","Çiğdem","Woman");
-        Student student9 = new Apple("5414322123","Zeynep","Woman");
+        Student student = new Student("5403633123","Bilal","Man");
+        Student student1 = new Student("5079705123","Fatih","Man");
+        Student student2 = new Student("5424363123","Gizem","Woman");
+        Student student3 = new Student("5414322123","Büşra","Woman");
+        Student student4 = new Student("5403633123","Ahmet","Man");
+        Student student5 = new Student("5079705123","Mustafa","Man");
+        Student student6 = new Student("5424363123","Merve","Woman");
+        Student student7 = new Student("5414322123","Ceyda","Woman");
+        Student student8 = new Student("5414322123","Çiğdem","Woman");
+        Student student9 = new Student("5414322123","Zeynep","Woman");
 
         student.getPersonByGender("Man");
         student.getPersonByGender("Woman");
